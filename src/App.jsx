58d3e7594f2fa9255b6cb9499498ac3e32c42d2f
Sellucas <lucassellmachado@gmail.com>
2023-02-01 15:03:47 -0300

    Updating and Editing task function

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,66 @@ import { useState } from "react";
 
 //Custom componentes
 import CustomForm from "./components/CustomForm";
+import EditForm from "./components/EditForm";
 import TaskList from "./components/TaskList";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [editedTask, setEditedTask] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [previousFocusElement, setPreviousFocusElement] = useState(null);
 
   const addTask = (task) => {
     setTasks((prevState) => [...prevState, task]);
   };
 
+  const deleteTask = (id) => {
+    setTasks((prevState) => prevState.filter((t) => t.id !== id));
+  };
+
+  const toggleTask = (id) => {
+    setTasks((prevState) =>
+      prevState.map((t) => (t.id === id ? { ...t, checked: !t.checked } : t))
+    );
+  };
+
+  const updateTask = (task) => {
+    setTasks((prevState) =>
+      prevState.map((t) => (t.id === task.id ? { ...t, name: task.name } : t))
+    );
+    closeEditMode();
+  };
+
+  const closeEditMode = () => {
+    setIsEditing(false);
+    previousFocusElement.focus();
+  };
+
+  const enterEditMode = (task) => {
+    setEditedTask(task);
+    setIsEditing(true);
+    setPreviousFocusElement(document.activeElement);
+  };
+
   return (
     <div className="container">
       <header>My Task List</header>
+      {isEditing && (
+        <EditForm
+          editedTask={editedTask}
+          updateTask={updateTask}
+          closeEditMode={closeEditMode}
+        />
+      )}
       <CustomForm addTask={addTask} />
-      {tasks && <TaskList tasks={tasks} />}
+      {tasks && (
+        <TaskList
+          tasks={tasks}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+          enterEditMode={enterEditMode}
+        />
+      )}
     </div>
   );
 }
